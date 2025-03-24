9f46eb8858ed5349d35e8b501a35e1756a7400d0
morgot <feodor.a.v.03@gmail.com>
2025-03-24 08:46:38 +0300

    html response video keys

--- api/keywords/view.py
+++ api/keywords/view.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Query, HTTPException, status, Body, Header, Depends
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, HTMLResponse
 from core.models import db_helper
 from sqlalchemy.ext.asyncio import AsyncSession
 from .crud import *
@@ -10,4 +10,4 @@ router = APIRouter(prefix="/keywords", tags=["Keywords"])
 @router.get("/video", response_model=str)
 async def rand_video_keyword(session: AsyncSession = Depends(db_helper.session_dependency)):
     res = await get_random_video_keyword(session=session)
-    return res.text
+    return HTMLResponse(res.text)
